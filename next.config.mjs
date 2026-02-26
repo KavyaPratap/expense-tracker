024a77a94aeb1c042211972d16815ef8e204ca25
Arsh Mishra <arshm0312@gmail.com>
2026-02-27 02:33:47 +0530

    Remove explicit webpack flag from Next.js dev/build scripts and update TypeScript target to esnext.

--- next.config.mjs
+++ next.config.mjs
@@ -4,15 +4,6 @@ const nextConfig = {
     typescript: {
         ignoreBuildErrors: true,
     },
-    webpack: (config) => {
-        config.module.rules.push({
-            test: /\.m?js$/,
-            resolve: {
-                fullySpecified: false,
-            },
-        });
-        return config;
-    },
 };
 
 export default nextConfig;
